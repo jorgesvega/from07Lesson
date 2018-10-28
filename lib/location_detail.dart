f661d540caa7c7cbcea624f5db33c1000a9979e9
Jorge Sánchez Vega <getmep@gmail.com>
2018-10-28 13:47:31 +0100

    Lesson 5 improvements

--- lib/location_detail.dart
+++ lib/location_detail.dart
@@ -14,12 +14,19 @@ class LocationDetail extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: _renderFacts(location),
+        children: _renderBody(context, location),
       ),
     );
   }
 
-  List<Widget> _renderFacts(Location location) {
+  List<Widget> _renderBody(BuildContext context, Location location) {
+    var result = List<Widget>();
+    result.add(_bannerImage(location.url, 170.0));
+    result.addAll(_renderFacts(context, location));
+    return result;
+  }
+
+  List<Widget> _renderFacts(BuildContext context, Location location) {
     var result = List<Widget>();
     for (int i = 0; i < location.facts.length; i++) {
       result.add(_sectionTitle(location.facts[i].title));
@@ -29,11 +36,30 @@ class LocationDetail extends StatelessWidget {
   }
 
   Widget _sectionTitle(String text) {
-    return Text(text);
+    return Container(
+      padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 10.0),
+      child: Text(text,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontSize: 25.0,
+          color: Colors.black,
+        ),
+      ),
+    );
   }
 
   Widget _sectionText(String text) {
-    return Text(text);
+    return Container(
+      padding: EdgeInsets.fromLTRB(25.0, 15.0, 25.0, 15.0),
+      child: Text(text),
+    );
+  }
+
+  Widget _bannerImage(String url, double height) {
+    return Container(
+      constraints: BoxConstraints.tightFor(height: height),
+      child: Image.network(url, fit: BoxFit.fitWidth),
+    );
   }
 
 }
