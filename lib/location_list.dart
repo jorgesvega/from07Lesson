16f7f58591e0773da05fdf6f9dce12a963d4aef1
Jorge Sánchez <getmep@gmail.com>
2018-10-31 19:47:03 +0100

    Lesson 7 Improvements

--- lib/location_list.dart
+++ lib/location_list.dart
@@ -11,7 +11,27 @@ class LocationList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("Locations", style: Styles.navBarTitle)),
-        body: null,
+        body: ListView.builder(
+          itemCount: this.locations.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              contentPadding: EdgeInsets.all(10.0),
+              leading: _itemThumbnail(this.locations[index]),
+              title: _itemTitle(this.locations[index]),
+            );
+          },
+        ),
     );
+  }
+
+  Widget _itemThumbnail(Location location) {
+    return Container(
+      constraints: BoxConstraints.tightFor(width: 100.0),
+      child: Image.network(location.url, fit: BoxFit.fitWidth)
+    );
+  }
+
+    Widget _itemTitle(Location location) {
+    return Text('>> ${location.name}', style: Styles.textDefault);
   }
 }
